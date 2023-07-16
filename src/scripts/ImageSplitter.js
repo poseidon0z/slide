8e5e05a8d2b99d21e813f0ecbf77c2e0e9b06b48
poseidon0z <adityaprabhu1290@gmail.com>
2023-07-16 16:30:18 +0530

    Added assets and image splitter

---
+++ src/scripts/ImageSplitter.js
@@ -0,0 +1,24 @@
+function loadCanvas() {
+    console.log("Image splitter started.");
+
+    const n = 3;
+
+    for (let i = 0; i < 9; i++){
+        const img = new Image();
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext("2d");
+        
+        img.onload = () => {
+            const height = img.naturalHeight;
+            const width = img.naturalWidth;
+            canvas.height = height/n;
+            canvas.width = width/n;
+            ctx.drawImage(img, (i%n) * width/n , (Math.floor(i/n)) * height/n, height/n, width/n, 0, 0, height/n, width/n);
+            const dataurl = canvas.toDataURL();
+            sessionStorage.setItem("img" + i, dataurl);
+        };
+        img.src = "src/assets/square.jpg";
+    }
+    console.log("Image splitter completed.")
+}
+loadCanvas();
